54b7c38ecd6b5ed6ed162c8b4d41968fcbed5dcc
rezaulfahim806-sys <rezaulfahim806@gmail.com>
2026-09-10 23:54:45 +0600

    Fix Android back navigation for SPA settings and screens

--- app/src/main/java/com/starcommunication/isp/MainActivity.java
+++ app/src/main/java/com/starcommunication/isp/MainActivity.java
@@ -5,34 +5,29 @@ import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.webkit.WebResourceRequest;
+import android.os.Build;
 
 public class MainActivity extends Activity {
-
     private WebView webView;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         webView = new WebView(this);
         setContentView(webView);
-
-        WebSettings settings = webView.getSettings();
-        settings.setJavaScriptEnabled(true);
-        settings.setDomStorageEnabled(true);
-        settings.setDatabaseEnabled(true);
-
-        webView.setWebViewClient(new WebViewClient());
-
+        WebSettings s = webView.getSettings();
+        s.setJavaScriptEnabled(true);
+        s.setDomStorageEnabled(true);
+        s.setDatabaseEnabled(true);
+        s.setAllowFileAccess(true);
+        s.setAllowContentAccess(true);
+        webView.setWebViewClient(new WebViewClient(){
+            @Override public boolean shouldOverrideUrlLoading(WebView v, WebResourceRequest r){ return false; }
+        });
         webView.loadUrl("file:///android_asset/index.html");
     }
-
-    @Override
-    public void onBackPressed() {
-        if (webView.canGoBack()) {
-            webView.goBack();
-        } else {
-            super.onBackPressed();
-        }
+    @Override public void onBackPressed() {
+        if (webView != null) {
+            webView.evaluateJavascript("typeof appBack==='function' ? appBack() : null", null);
+        } else super.onBackPressed();
     }
 }
