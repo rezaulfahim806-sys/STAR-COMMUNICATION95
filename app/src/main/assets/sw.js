a4ba4b177d10dfe7bcf035a503494d9b69c50e8d
rezaulfahim806-sys <rezaulfahim806@gmail.com>
2026-09-11 17:29:30 +0600

    Add offline web app service worker

---
+++ app/src/main/assets/sw.js
@@ -0,0 +1,4 @@
+const CACHE='star-communication-v1';
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./index.html','./logo.svg','./manifest.webmanifest']))));
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match('./index.html')))));
